2a41c92952ca0d5b2446b2bc3c8fc01f3babbbc2
Balazs Juhasz <juhaszbal@gmail.com>
2020-12-12 17:51:23 +0100

    finish selenium tutorial

--- selenium/testcase/locator.py
+++ selenium/testcase/locator.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
     GO_BUTTON = (By.ID, "submit")
+
+
+class SearchResultsPageLocators(object):
+    pass
--- selenium/testcase/main.py
+++ selenium/testcase/main.py
@@ -10,6 +10,15 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome(
             "/home/balazs/chrome-driver/chromedriver")
         self.driver.get("http://www.python.org")
+    
+    def test_search_python(self):
+        mainPage = page.MainPage(self.driver)
+        assert mainPage.is_title_matches()
+        mainPage.search_text_element = "pycon"
+        mainPage.click_go_button()
+        search_results_page = page.SearchResultPage(self.driver)
+        assert search_results_page.is_results_found()
+
 
     def tearDown(self):
         self.driver.close()
