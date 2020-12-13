fb901a65dd94f48196c0e6de44311908cf3758c2
Balazs Juhasz <juhaszbal@gmail.com>
2020-12-13 10:31:12 +0100

    avoid regenerating random number

--- randomgame.py
+++ randomgame.py
@@ -3,12 +3,13 @@ import random
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
+number = random.randint(start, end)
 
 while True:
     answer = int(input(f'Guess a number between {start} and {end}: '))
-    number = random.randint(start, end)
+
     if answer == number:
         print('You guessed correctly.')
         break
 
-    print(f'Random number was: {number}, please try again')
+    print(f'Wrong answer, please try again')
